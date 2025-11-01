d228a44644f3ccf384ad5256bede0903b2b71191
Elaine Fabiola Soares <elaine.fabiola@gmail.com>
2025-11-01 12:05:10 -0300

    feat: implementa endpoint GET /usuarios/filtro-imc para filtrar por faixa de IMC

---
+++ biotrack/src/main/java/com/ProgWebII/biotrack/controller/UsuarioController.java
@@ -0,0 +1,41 @@
+package com.ProgWebII.biotrack.controller;
+
+import com.ProgWebII.biotrack.model.User;
+import com.ProgWebII.biotrack.model.Imc;
+import com.ProgWebII.biotrack.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private Imc imc;
+
+    @GetMapping("/filtro-imc")
+    public ResponseEntity<List<User>> filtrarUsuariosPorImc(@RequestParam String faixa) {
+        List<User> todosUsuarios = userRepository.findAll();
+        
+        List<User> usuariosFiltrados = todosUsuarios.stream()
+            .filter(user -> {
+                Double imcUsuario = imc.obterImcUsuario(user.getId());
+                if (imcUsuario == null) {
+                    return false;
+                }
+                String faixaUsuario = imc.classificarFaixaImc(imcUsuario);
+                return faixaUsuario != null && faixaUsuario.equalsIgnoreCase(faixa);
+            })
+            .collect(Collectors.toList());
+        
+        return ResponseEntity.ok(usuariosFiltrados);
+    }
+}
+
